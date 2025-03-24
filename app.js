8856f038e8e9f2fba48a7ed2de51615aadd2748d
GuustavoPetry <ogustavopetry@gmail.com>
2025-03-24 12:17:04 -0300

    enjanbrando

--- app.js
+++ app.js
@@ -5,29 +5,27 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-
 // Middleware para servir arquivos estáticos
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use((req, res) => {
-  res.status(404).sendFile(__dirname + '/public/thank_you.html');
-});
-
-app.post('/feedback', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'thank_you.html'));
-});
-
 
 // Rota para a página principal
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+// Rota para o formulário de feedback
+app.post('/feedback', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'thank_you.html'));
+});
+
+// Middleware para capturar erros 404 (deve ser a ÚLTIMA configuração)
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(__dirname, 'public', 'thank_you.html'));
+});
+
 // Iniciar o servidor
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
 });
-
-
-
